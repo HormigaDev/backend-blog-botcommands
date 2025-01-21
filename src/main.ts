ad589dd1b7e361b110f685abbab9ac6bddea505c
Isai Medina <isai@zettabrasil.com.br>
2025-01-21 09:28:14 -0300

    feat: activado las cors para desarrollo

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.use(cookieParser());
+    if (process.env.NODE_ENV === 'development') {
+        app.enableCors();
+    }
     app.useGlobalPipes(
         new ValidationPipe({
             transform: true,
